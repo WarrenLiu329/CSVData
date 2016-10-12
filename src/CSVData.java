161b39c4bb2d1d0cb43436c9e110012af3f24d32
L.B. Warren Liu <warrenliu403@gmail.com>
2016-10-12 10:57:44 -0700

    Create getters and setters for adding and getting the values in the data

--- src/CSVData.java
+++ src/CSVData.java
@@ -7,21 +7,54 @@
 public class CSVData {
 	private String[] columnHeaders;
 	private double[][] data;
-	
-	public static CSVData readCSVFile(String fileName, int numLinesToIgnore, String[] columnNames){
+
+	public static CSVData readCSVFile(String fileName, int numLinesToIgnore, String[] columnNames) {
 		return null;
 	}
+
 	/***
-	 * Returns a new CSVData object for a file ignoring lines at the top.
-	 * It uses the first row as the column names. All other data is stored
-	 * as doubles.
+	 * Returns a new CSVData object for a file ignoring lines at the top. It
+	 * uses the first row as the column names. All other data is stored as
+	 * doubles.
 	 * 
 	 * @param fileName
 	 * @param numLinestoIgnore
 	 * @return
 	 */
-	public CSVData readCSVFile(String fileName, int numLinestoIgnore){
+	public CSVData readCSVFile(String fileName, int numLinestoIgnore) {
 		return null;
 	}
-	
+
+	public int getRow(int r) {
+		return r;
+	}
+
+	public int getColumn(int c) {
+		return c;
+	}
+
+	public double[][] getMultipleRows(double[][] data, int startRow, int endRow) {
+		return null;
+	}
+
+	public double[][] getMultipleColumns(double[][] data, int startColumn, int endColumn) {
+		return null;
+	}
+
+	public double[][] getData() {
+		return this.data;
+	}
+
+	public String[] getColumnTitles() {
+		return this.columnHeaders;
+	}
+
+	public static void setColumn(double[][] data, int c, double[] values) {
+
+	}
+
+	public static void detRow(double[][] data, int r, double[] values) {
+
+	}
+
 }
